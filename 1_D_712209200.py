2c9c958c99d322fb86341dbe0735e583ae9c0de2
Rio-arbanianto <117901421+Rio-arbanianto@users.noreply.github.com>
2022-12-01 16:18:09 +0700

    Add files via upload

---
+++ 1_D_712209200.py
@@ -0,0 +1,32 @@
+
+def salam ():
+    print("selamat datang di bioskop")
+    print("dimanakah kamu ingin menonton?")
+    print("1) XXI Empire")
+    print("2) XXI Amplas")
+    print("3) XXI JCM")
+salam()
+
+pil = int(input("Masukan pilihanmu: "))
+if pil == 1:
+    print("1. Frozen")
+    print("2. Keramat")
+    print("3. KKN di desa penari ")
+  
+elif pil == 2:
+    print("1. Reguler")
+    print("2. Dolby almos")
+    print("3. premiere ")
+  
+elif pil == 3:
+     print("1. 12.35")
+     print("2. 14.45")
+     print("3. 16.55")
+     print("4. 19.05")
+
+elif pil >= 4:
+    print("pilihan tidak tersedia")
+    
+
+
+
